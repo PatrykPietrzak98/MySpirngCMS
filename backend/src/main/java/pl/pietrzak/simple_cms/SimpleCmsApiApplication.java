dfd0a9aa7ee8521382d20d33dd32d31496dad1aa
Patryk Pietrzak <p.pietrzak998@gmail.com>
2024-06-04 05:16:02 +0200

    initial commit

---
+++ backend/src/main/java/pl/pietrzak/simple_cms/SimpleCmsApiApplication.java
@@ -0,0 +1,13 @@
+package pl.pietrzak.simple_cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCmsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCmsApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pl/pietrzak/simple_cms/SimpleCmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pietrzak.simple_cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCmsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
