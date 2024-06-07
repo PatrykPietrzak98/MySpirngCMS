fd7db8ee560dc1df33877bac0a5add2016072cfb
Patryk Pietrzak <p.pietrzak998@gmail.com>
2024-06-07 20:12:05 +0200

    Added testing by spock

---
+++ backend/src/test/groovy/pl/pietrzak/simple_cms/SimpleCmsApiApplicationSpec.groovy
@@ -0,0 +1,17 @@
+package pl.pietrzak.simple_cms
+
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class SimpleCmsApiApplicationSpec extends Specification {
+    def "test always passing"(){
+        expect:
+        true
+    }
+
+    def "test always false"(){
+        expect:
+        false
+    }
+}
